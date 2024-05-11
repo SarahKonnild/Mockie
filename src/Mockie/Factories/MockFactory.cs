68b9bbdc10884bc37b4fda9ba6522071e27e3a81
SarahKnapp_Mac <S_Pradel@icloud.com>
2024-05-11 09:47:57 +0200

    How to generate methods on a dynamic class noted down.

--- src/Mockie/Factories/MockFactory.cs
+++ src/Mockie/Factories/MockFactory.cs
@@ -1,9 +1,6 @@
-﻿using Mockie.Types;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Reflection;
+using System.Reflection.Emit;
+using Mockie.Types;
 
 namespace Mockie.Factories
 {
@@ -11,6 +8,39 @@ namespace Mockie.Factories
     {
         public Mock<T> CreateMock<T>() where T : class 
         {
+            // Create a new dynamic assembly
+            AssemblyName assemblyName = new AssemblyName("DynamicAssembly");
+            AssemblyBuilder assemblyBuilder =
+                AssemblyBuilder.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.Run);
+            ModuleBuilder moduleBuilder = assemblyBuilder.DefineDynamicModule("MainModule");
+            
+            // Define a new type dynamically
+            TypeBuilder typeBuilder = moduleBuilder.DefineType("DynamicClass", TypeAttributes.Public);
+            
+            // Implement the interface in the dynamic class
+            typeBuilder.AddInterfaceImplementation(typeof(T));
+            
+            // Get the methods from the source type and add them to the new type
+            foreach (MethodInfo methodInfo in typeof(T).GetMethods())
+            {
+                // Define the method in the new type
+                MethodBuilder methodBuilder = typeBuilder.DefineMethod(
+                    methodInfo.Name,
+                    MethodAttributes.Public | MethodAttributes.Virtual,
+                    methodInfo.ReturnType,
+                    Array.ConvertAll(methodInfo.GetParameters(), p => p.ParameterType)
+                );
+                
+                // Generate IL code to throw NotImplementedException for each method
+                ILGenerator ilGenerator = methodBuilder.GetILGenerator();
+                ilGenerator.Emit(OpCodes.Newobj, typeof(NotImplementedException).GetConstructor(Type.EmptyTypes));
+                ilGenerator.Emit(OpCodes.Throw);
+                
+                // Complete the method definition
+                typeBuilder.DefineMethodOverride(methodBuilder, methodInfo);
+            }
+            
+            // return typeBuilder.CreateType();
             return new Mock<T>();
         }
     }
--- src/Mockie/Types/Mock.cs
+++ src/Mockie/Types/Mock.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Mockie.Types
+﻿namespace Mockie.Types
 {
     internal class Mock<T> where T : class
     {
